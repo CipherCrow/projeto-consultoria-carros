33eee020571dfb8de6c6b624dabc8b2dbf54f151
Jonas Torquatro <60902980+CipherCrow@users.noreply.github.com>
2021-03-27 20:46:11 -0300

    :up: preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const volante = document.querySelector('.preloader');
+
+window.addEventListener('load',function(){
+	volante.style.opacity = '0';
+	setTimeout(function(){
+		volante.style.display = 'none';
+	}, 300);
+})
+
 const navMobile = document.querySelector('.mobile i');
 
 navMobile.addEventListener('click',function(){
